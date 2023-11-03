cd844666e1c94c51315a3d6886ddba9fe2bde08c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-11-03 18:37:15 +0100

    !!

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -9,14 +9,14 @@
           @csrf
           <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="John Doe">
+            <input type="text" class="form-control" id="name" name="name" placeholder="John Doe" value="{{old('name')}}">
             @error('name')
                 <p class="text-danger mb-2">{{$message}}</p>
             @enderror
           </div>
           <div class="form-group">
             <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" value="{{old('email')}}">
             @error('email')
             <p class="text-danger mb-2">{{$message}}</p>
            @enderror
@@ -34,7 +34,7 @@
           </div>
           <div class="form-group">
             <label for="bio">Bio</label>
-            <textarea class="form-control" id="bio" name="bio" rows="3" placeholder="write what's on your mind..."></textarea>
+            <textarea class="form-control" id="bio" name="bio" rows="3" placeholder="write what's on your mind...">{{old('name')}}</textarea>
             @error('bio')
             <p class="text-danger mb-2">{{$message}}</p>
              @enderror
