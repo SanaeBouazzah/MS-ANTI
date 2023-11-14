3f350f7b2538737520a62c34a859410ba4fbeccd
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-11-14 19:14:29 +0100

    !!

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -2,45 +2,28 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\Profile;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    public function index()
+    public function index()  
     {
-        $profiles = Profile::all();
-        return response()->json($profiles);
+        return Profile::paginate(2);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Profile $profile)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Profile $profile)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Profile $profile)
     {
         //
