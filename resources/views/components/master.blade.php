26a8269419753d3f1900c7ee9340de289321a6bb
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-11-18 14:04:25 +0100

    !!

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -875,6 +875,7 @@
             @include('partials.footer')
         </div>
     </main>
+    <h3>hello</h3>
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
